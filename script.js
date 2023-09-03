cecca41da6b02d5701670aacfc9e476b31b02d6c
Kiameg <kiax1262@stud.kea.dk>
2023-09-03 14:47:54 +0200

    profanity1

---
+++ script.js
@@ -0,0 +1,5 @@
+let theText = "jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
+// replace the word 'ord' with the word 'ting'
+theText = theText.replaceAll("ord", "ting");
+//console log the result
+console.log(theText);
